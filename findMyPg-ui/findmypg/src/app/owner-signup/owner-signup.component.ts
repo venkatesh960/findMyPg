c228c64ad2f3ff955aa166c37a2e599b603626c1
Venkatesh4A4 <venkateshbottupalli960@gmail.com>
2024-06-17 13:03:39 +0530

    Added angular ui

---
+++ findMyPg-ui/findmypg/src/app/owner-signup/owner-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-signup',
+  templateUrl: './owner-signup.component.html',
+  styleUrl: './owner-signup.component.scss'
+})
+export class OwnerSignupComponent {
+
+}
---
+++ findMyPg-ui/findmypg/src/app/user-header/user-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-header',
+  templateUrl: './user-header.component.html',
+  styleUrls: ['./user-header.component.scss']
+})
+export class UserHeaderComponent {
+}
