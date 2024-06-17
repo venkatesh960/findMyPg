38def5fd868d5ec721520d66b71bbbd608be0119
Venkatesh4A4 <venkateshbottupalli960@gmail.com>
2024-06-17 11:09:44 +0530

    Added employee, student

--- src/main/java/com/example/findmypg/owner/OwnerRegService.java
+++ src/main/java/com/example/findmypg/owner/OwnerRegService.java
@@ -1,29 +1,31 @@
 package com.example.findmypg.owner;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.findmypg.entities.OwnerRegistration;
-
+import com.example.findmypg.entities.Owner;
 
 @Service
 public class OwnerRegService {
-	
-	@Autowired 
+
+	@Autowired
 	private OwnerRegistrationRepo ownerRegiRepo;
-	
+
 	public Boolean ownerRegistration(OwnerRegDTO ownerDTO) {
-	
-		OwnerRegistration ownerRegistration=new OwnerRegistration();
+
+		Owner ownerRegistration = new Owner();
 		ownerRegistration.setFirstName(ownerDTO.getFirstName());
 		ownerRegistration.setLastName(ownerDTO.getLastName());
 		ownerRegistration.setMiddleName(ownerDTO.getMiddleName());
 		ownerRegistration.setMobileNum(ownerDTO.getMobileNumber());
 		ownerRegistration.setPassword(ownerDTO.getPassword());
+		ownerRegistration.setEmail_Id(ownerDTO.getEmailId());
 		ownerRegistration.setUserName(ownerDTO.getUserName());
-		
-		OwnerRegistration check=ownerRegiRepo.save(ownerRegistration);
-		if (check!=null) {
+
+		Owner check = ownerRegiRepo.save(ownerRegistration);
+		if (check != null) {
 			return true;
 		} else {
 			return false;
@@ -31,10 +33,33 @@ public class OwnerRegService {
 	}
 
 	public OwnerRegDTO getOwnerDetails(String mobileNumber) {
-//		System.out.println(ownerRegiRepo.findById(1l));
-		OwnerRegistration ownerRegistration=ownerRegiRepo.findByMobileNum(mobileNumber);
-		System.out.println(ownerRegistration.toString());
-		return null;
+
+		Owner owner = ownerRegiRepo.findByMobileNum(mobileNumber);
+
+		if (owner != null) {
+			OwnerRegDTO dto = new OwnerRegDTO();
+			dto.setFirstName(owner.getFirstName());
+			dto.setLastName(owner.getLastName());
+			dto.setEmailId(owner.getEmail_Id());
+			dto.setMiddleName(owner.getMiddleName());
+			dto.setUserName(owner.getUserName());
+			dto.setPassword(owner.getPassword());
+			return dto;
+		} else {
+			return null;
+		}
+	}
+
+	public boolean loginChcek(OwnerRegDTO dto) {
+		List<Owner> list=ownerRegiRepo.findAll();
+		if (list!=null) {
+			for (Owner owner : list) {
+				if (owner.getMobileNum().equalsIgnoreCase(dto.getMobileNumber()) && owner.getUserName().equalsIgnoreCase(dto.getUserName())) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
