39fbf501a44fdd23f5fa1af6f117496725de05f5
Venkatesh4A4 <venkateshbottupalli960@gmail.com>
2024-06-22 21:21:54 +0530

    add some components

---
+++ findMyPg-ui/findmypg/src/app/owner-screen/owner-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-screen',
+  templateUrl: './owner-screen.component.html',
+  styleUrl: './owner-screen.component.scss'
+})
+export class OwnerScreenComponent {
+
+}
