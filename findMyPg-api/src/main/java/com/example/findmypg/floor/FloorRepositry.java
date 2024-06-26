4219358d6ce35bffaf13363e245c181d8058bfc3
Venkatesh4A4 <venkateshbottupalli960@gmail.com>
2024-06-27 00:48:27 +0530

    Added controller to get buildings, floors

--- findMyPg-api/src/main/java/com/example/findmypg/floor/FloorRepositry.java
+++ findMyPg-api/src/main/java/com/example/findmypg/floor/FloorRepositry.java
@@ -1,9 +1,13 @@
 package com.example.findmypg.floor;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.findmypg.entities.Floor;
 
 public interface FloorRepositry extends JpaRepository<Floor, Long>{
+
+	List<Floor> findByBuilding_Id(Long buildingId);
 
 }
