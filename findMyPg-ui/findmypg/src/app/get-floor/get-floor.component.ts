dd7c0c6f8c86e28cf893ccabfd81739979c202c4
Venkatesh4A4 <venkateshbottupalli960@gmail.com>
2024-07-06 11:20:14 +0530

    some ui componets for displaying building details

--- findMyPg-ui/findmypg/src/app/get-floor/get-floor.component.ts
+++ findMyPg-ui/findmypg/src/app/get-floor/get-floor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OwnerServiceService } from '../owner-service.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-get-floor',
@@ -7,12 +9,27 @@ import { OwnerServiceService } from '../owner-service.service';
   styleUrl: './get-floor.component.scss'
 })
 export class GetFloorComponent implements OnInit{
-  public constructor(private ownerService:OwnerServiceService)
+  floorsdata:any
+  public constructor(private router:Router,private httpClient:HttpClient,private ownerService:OwnerServiceService)
   {
     
   }
   ngOnInit(): void {
    
+    this.getFloorDetails();
+  }
+
+  getFloorDetails():void{
+    this.httpClient.get(`/api/findmypg/floor/getListOfFloors?buildingId=${1}`).subscribe(response=>{
+      if (response!=null) {
+        console.log("successfully got response from get floor ",response);
+        this.floorsdata=response;
+        
+      } else {
+        console.log("something went wrong by getting floor details ",response);
+        
+      }
+    });
   }
 
 }
