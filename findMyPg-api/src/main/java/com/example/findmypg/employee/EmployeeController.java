9c5ea9663b27e99528c5478f4ea6cde2bb9673fe
Bvenkatesh960 <venkateshbottupalli960@gmail.com>
2024-12-09 22:22:27 +0530

    add extra files

--- findMyPg-api/src/main/java/com/example/findmypg/employee/EmployeeController.java
+++ findMyPg-api/src/main/java/com/example/findmypg/employee/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +32,11 @@ public class EmployeeController {
 	@DeleteMapping(value = "/removeEmployee")
 	private int removeEmployee(@RequestParam Long employeeId) {
 		return employeeService.removeEmployee(employeeId);
+	}
+	
+	@PutMapping(value = "/updateEmployee")
+	private int updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+		return employeeService.updateEmployee(employeeDTO);
 	}
 
 }
